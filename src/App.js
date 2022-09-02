f39d72a073720a5b4f32050391cf599ca07a6195
shakil1 <shakilahamed1624@gmail.com>
2022-09-02 11:38:17 +0600

    developer section added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './components/About';
+import Developer from './components/Developer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 
@@ -9,6 +10,7 @@ const App = () => {
      <Navbar></Navbar>
      <Hero></Hero>
      <About></About>
+     <Developer></Developer>
       
     </div>
   );
