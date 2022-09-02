c15d0e06f52b574c0f35f6d305786c23ab1c6790
shakil1 <shakilahamed1624@gmail.com>
2022-09-02 11:49:07 +0600

    subscribe section added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/About';
 import Developer from './components/Developer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Subscribe from './components/Subscribe';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
      <Hero></Hero>
      <About></About>
      <Developer></Developer>
+     <Subscribe></Subscribe>
       
     </div>
   );
